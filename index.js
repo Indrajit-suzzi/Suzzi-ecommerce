d146882ea9ae8fee0a7240dd88a64855b4c7e14e
indrajit-suzzi <indrajitroy8536@gmail.com>
2024-09-24 16:56:11 +0530

    404_page_add

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.get("/home", (req, res) => {
     res.render("home.ejs");
 })
 
+app.use((req, res, next) => {
+    res.status(404).render('404.ejs');
+  });
+
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 })
