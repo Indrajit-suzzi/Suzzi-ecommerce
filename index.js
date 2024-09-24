e5f03d669e8a5deb68c461319efa82957371fb99
indrajit-suzzi <indrajitroy8536@gmail.com>
2024-09-25 00:01:34 +0530

    for mobile 404

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get("/home", (req, res) => {
 })
 
 app.use((req, res, next) => {
-    res.status(404).render('404.ejs');
+    res.status(404).render('errorPage.ejs');
   });
 
 app.listen(port, () => {
