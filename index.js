ff84bc1ccee017c93f50e2bb9ff4cd2566454dac
indrajit-suzzi <indrajitroy8536@gmail.com>
2024-11-23 21:03:54 +0530

    a

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
 const express = require("express");
+const path = require("path");
 const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-
-const baseRoutes = require("./routes/baseRoutes"); 
-const loginRoutes = require("./routes/loginRoutes"); 
-
 dotenv.config();
-
 const app = express();
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+const baseRoutes = require("./routes/baseRoutes");
+const loginRoutes = require("./routes/loginRoutes");
+
 // Database connection
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
-    if (err) {
-        console.error("Error connecting to the database:", err);
-        process.exit(1);
-    }
-    console.log("Connected to the MySQL database!");
+  if (err) {
+    console.error("Error connecting to the database:", err);
+    process.exit(1);
+  }
+  console.log("Connected to the MySQL database!");
 });
 
-// Use the routes
+//routes
 app.use("/", baseRoutes);
-app.use("/", loginRoutes); 
+app.use("/", loginRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = db;
